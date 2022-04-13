bc90f1f4660cdfee267e225927d3d654b6721640
Rutuja243 <75740523+Rutuja243@users.noreply.github.com>
2022-04-13 19:22:21 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\sanjivanibot1-Web-Deploy.pubxml -p:Password=JWtq5pmlpsqSXolwbSkNhLgxexCPij8ugY9EsD7cK6nbpbyZLX0G6mQoJKzk
+
